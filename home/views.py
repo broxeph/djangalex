e1125b4787e2e499e7beab3e93e19c72597ccf29
Alex Ball <alex@ameryn.com>
2015-12-03 23:49:15 -0500

    Add index.html template; tweak models

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Box
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Alex index.")
+    boxes = Box.objects.all()
+    context = {'boxes': boxes}
+    return render(request, 'home/index.html', context)
