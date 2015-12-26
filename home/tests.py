70236cf35300c1dbddcfa8c1d475b7377563acc4
Alex Ball <alex@ameryn.com>
2015-12-26 15:28:42 -0500

    Added logo_url_static test

--- home/tests.py
+++ home/tests.py
@@ -5,10 +5,11 @@ from .models import Box
 
 class BoxTestCase(TestCase):
     def setUp(self):
-        Box.objects.create(name='testBox', logo_url='testLogo',
+        Box.objects.create(name='testBox', logo_url='testLogo.png',
             link_url='http://example.com', sort_order=0)
 
     def test_box_something(self):
         """Testing basic box creation"""
         testBox = Box.objects.get(name='testBox')
         self.assertEqual(testBox.name, 'testBox')
+        self.assertEqual(testBox.logo_url_static(), 'home/testLogo.png')
