c74476f0ece29614c64bb0a9430ea0efbfe5fdb9
Alex Ball <alex@ameryn.com>
2016-01-20 00:00:20 -0500

    Home header column md -> sm

--- home/tests.py
+++ home/tests.py
@@ -5,10 +5,11 @@ from .models import Box
 
 class BoxTestCase(TestCase):
     def setUp(self):
-        Box.objects.create(name='testBox', logo_url='testLogo.png',
+        Box.objects.create(
+            name='testBox', logo_url='testLogo.png',
             link_url='http://example.com', sort_order=0)
 
-    def test_box_something(self):
+    def test_box_creation(self):
         """Testing basic box creation"""
         testBox = Box.objects.get(name='testBox')
         self.assertEqual(testBox.name, 'testBox')
