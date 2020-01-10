d4fd779df1ba5583db8fd38c83b308244a2ba632
Alex Ball <alex@ball.lol>
2020-01-09 20:00:33 -0500

    Remove django-suit (doesn't support Django 3.0)

--- home/apps.py
+++ home/apps.py
@@ -1,10 +1,5 @@
 from django.apps import AppConfig
-from suit.apps import DjangoSuitConfig
 
 
 class HomeConfig(AppConfig):
     name = 'home'
-
-
-class SuitConfig(DjangoSuitConfig):
-    layout = 'horizontal'
